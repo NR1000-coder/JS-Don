66f74e6f3c35ea46239eccc71893cc28fcd3f231
NR1000-coder <nrminseyfullayeva@gmail.com>
2025-06-20 15:54:16 +0000

    jsDone

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-"use strict";
 $(document).ready(function () {
 
+    $('input').html('Please write' );
+
+    $('.btn').click(function (e) { 
+        e.preventDefault();
+        
+        let a = (a) => {
+            if (parseInt(a) + 0 == parseInt(a)) {
+                for (let i = 1; i<a; i++) {
+                    $('.minibox').append(`<h3>${a}</h3>`);
+                }
+            } 
+            if (a + 0 != parseInt(a)){
+                $('.minibox').append(`<h3>${a}</h3>`);
+            }
+        }
+        a($('input').val())
+    });
 });
+
