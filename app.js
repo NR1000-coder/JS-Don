bddd6b29315cc716d52d88dff85343330bc6d37e
NR1000-coder <nrminseyfullayeva@gmail.com>
2025-06-28 13:58:47 +0000

    condition23done

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 $(document).ready(function () {
 
-    $('input').html('Please write' );
-
+    $('input').click(function (e) { 
+        e.preventDefault();
+        
+        $('input').val("");
+    });
     $('.btn').click(function (e) { 
         e.preventDefault();
         
-        let a = (a) => {
-            if (parseInt(a) + 0 == parseInt(a)) {
-                for (let i = 1; i<a; i++) {
+        if ($.trim($("input").val()) != '') {
+            let a = (a) => {
+                if (parseInt(a) + 0 == parseInt(a)) {
+                    for (let i = 1; i<a; i++) {
+                        $('.minibox').append(`<h3>${a}</h3>`);
+                    }
+                } 
+                if (a + 0 != parseInt(a)){
                     $('.minibox').append(`<h3>${a}</h3>`);
-                }
-            } 
-            if (a + 0 != parseInt(a)){
-                $('.minibox').append(`<h3>${a}</h3>`);
+                }  
             }
-        }
         a($('input').val())
+        }
+        $('input').val("Please write");
     });
+
 });
 
